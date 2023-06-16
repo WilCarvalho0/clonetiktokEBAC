b6481812afda5189583668da50a15e4065938315
wilcarvalho <wil_carvalho0@icloud.com>
2023-06-15 21:41:32 -0300

    novo

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
-import './Video.css';
+import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter"
+import "./Video.css";
 
 const Video = () => {
   const videoRef = useRef(null);
@@ -18,14 +19,16 @@ const Video = () => {
   }
 
   return (
-    <div className='video'>
+    <div className="video">
       <video
-        className='video__player'
+        className="video__player"
         ref={videoRef}
         onClick={handleStart}
         loop
-        src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
       ></video>
+
+      <VideoFooter />
     </div>
   );
 };
