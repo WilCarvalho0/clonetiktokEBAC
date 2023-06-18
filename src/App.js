4dcc347611f6264898ecdacb9814051767cadd0e
wilcarvalho <wil_carvalho0@icloud.com>
2023-06-18 02:08:38 -0300

    novo2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-
+import React, { useEffect, useState } from "react";
 import './App.css';
-import Video from "./pages/Video"
+import VideoComponent from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map(doc => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
-    <div className='app__videos'>
-    <Video  
-    likes={100}
-    messages={200}
-    shares={300}
-    name="Will"
-    description="Brecker o goleiro"
-    music="música épica"
-    url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-    />
- <Video  
-    likes={111}
-    messages={111}
-    shares={111}
-    name="Gabi"
-    description=" Bird lindonaaa"
-    music="Clap your hands"
-    url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-    />
-
-    </div>
+      <div className='app__videos'>
+        {videos.map((item) => (
+          <VideoComponent
+            likes={item.likes}
+            messages={item.messages}
+            shares={item.shares}
+            name={item.name}
+            description={item.description}
+            music={item.music}
+            url={item.url}
+          />
+        ))}
+        
+      </div>
     </div>
   );
 }
